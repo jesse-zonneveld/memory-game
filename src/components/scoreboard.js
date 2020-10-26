29baa1625f037a25b114bd79889f4f19b1a16ed3
jesse-zonneveld <jessezonneveld@gmail.com>
2020-10-26 16:47:33 -0700

    remove unwanted react files, add ribbon to css

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -4,7 +4,7 @@ import "../scss/03-components/_scoreboard.scss";
 class Scoreboard extends Component {
     render() {
         return (
-            <div className="scoreboard">
+            <div className="scoreboard banner">
                 <div className="current-score-container">
                     <div className="current-score-title">
                         Current Score: {this.props.currentScore}
